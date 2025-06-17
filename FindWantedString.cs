aaa02287402f731bfbc3e3ba89662a975833035f
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-06-17 19:38:38 +0900

    [feat] 레포지토리 생성

---
+++ FindWantedString.cs
@@ -0,0 +1,53 @@
+﻿namespace PracticeCodeTest;
+
+public class FindWantedString
+{
+    public static int Solution1(string myString, string pat)
+    {
+        string lowMyString = myString.ToLower();
+        string lowPat = pat.ToLower();
+        int answer = 0;
+        
+        if (lowMyString.Contains(lowPat))
+        {
+            answer = 1;
+        }
+        
+        return answer;
+    }
+
+    public static int Solution2(string myString, string pat)
+    {
+        string lowMyString = myString.ToLower();
+        string lowPat = pat.ToLower();
+        int answer = 0;
+        bool isUnmatch = false;
+        
+        for (int i = 0; i < lowMyString.Length; i++)
+        {
+            for (int j = 0; j < lowPat.Length; j++)
+            {
+                if (myString.Length <= j + i)
+                {
+                    isUnmatch = true;
+                    break;
+                }
+                
+                if (lowMyString[i + j] != lowPat[j])
+                {
+                    isUnmatch = true;
+                    break;
+                }
+            }
+            
+            if(!isUnmatch)
+                return 1;
+            
+            isUnmatch = false;
+        }
+        
+        return answer;
+    }
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using PracticeCodeTest;
+
+public class MainClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FindWantedString.Solution1("aaAA", 	"aaaaa"));
+        Console.WriteLine(FindWantedString.Solution2("aaAA", 	"aaaaa"));
+    }
+}
