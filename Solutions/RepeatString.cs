34f22376fda99388dc28f9c6d1bf602e12851008
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-06-30 11:39:00 +0900

    [feat] 문자열 회전, 반복 문제 풀이

---
+++ Solutions/RepeatString.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticeCodeTest;
+
+public class RepeatString
+{
+    /*
+     * 문자열 str과 정수 n이 주어집니다.
+     * str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+     * 입력 : string 5
+     * 출력 : stringstringstringstringstring
+     */
+    
+    //https://school.programmers.co.kr/learn/courses/30/lessons/181950
+    
+    public static void Solution(string s, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(s);
+        }
+    }
+}
---
+++ Solutions/RotateString.cs
@@ -0,0 +1,26 @@
+﻿namespace PracticeCodeTest;
+
+public class RotateString
+{
+    /*
+     * 문자열 str이 주어집니다.
+     * 문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+     * 입력 : abcde
+     * 출력 :
+     * a
+     * b
+     * c
+     * d
+     * e
+     */
+    
+    //https://school.programmers.co.kr/learn/courses/30/lessons/181945
+    
+    public static void Solution(string s)
+    {
+        foreach (char c in s)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
