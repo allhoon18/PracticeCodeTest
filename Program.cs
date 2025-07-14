92504c3ca5fbd0b2a8174cd260849a80c5062852
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-07-14 15:30:19 +0900

    [feat] 올바른 괄호 문제 풀이

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ public class MainClass
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(FibonacciCalculator.Solution(5));
+        Console.WriteLine(BalancedParentheses.Solution("((()))"));
     }
 }
---
+++ Solutions/BalancedParentheses.cs
@@ -0,0 +1,43 @@
+﻿namespace PracticeCodeTest.Solutions;
+
+public class BalancedParentheses
+{
+    /*
+     * 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다.
+     * '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때,
+     * 문자열 s가 올바른 괄호이면 true를 return 하고,
+     * 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+     */
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+    public static bool Solution(string s)
+    {
+        int parenthesesOpenCount = 0;
+        bool isOpen = false;
+        
+        const char open = '(';
+        const char close = ')';
+
+        foreach (char c in s)
+        {
+            if (c == open)
+            {
+                parenthesesOpenCount++;
+                isOpen = true;
+            }
+            else if (c == close && isOpen)
+            {
+                parenthesesOpenCount--;
+                
+                if(parenthesesOpenCount == 0)
+                    isOpen = false;
+            }
+            else if (c == close && !isOpen)
+                return false;
+                
+        }
+        
+        return parenthesesOpenCount == 0;
+    }
+}
