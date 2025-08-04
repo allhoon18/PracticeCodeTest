aa92604d666ac81c15546139e5a261bf0c8f4b00
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-08-04 17:17:38 +0900

    [feat] 모음사전 문제 풀이

--- Program.cs
+++ Program.cs
@@ -6,11 +6,6 @@ public class MainClass
 {
     static void Main(string[] args)
     {
-        var arr = MathProdigyFinder.Solution([1,3,2,4,2]);
-
-        foreach (var item in arr)
-        {
-            Console.WriteLine(item);
-        }
+        Console.WriteLine(VowelDictionarySearch.Solution("AAAAE"));
     }
 }
---
+++ Solutions/VowelDictionarySearch.cs
@@ -0,0 +1,54 @@
+﻿namespace PracticeCodeTest.Solutions;
+
+public class VowelDictionarySearch
+{
+    /*
+     * 사전에 알파벳 모음 'A', 'E', 'I', 'O', 'U'만을 사용하여 만들 수 있는, 길이 5 이하의 모든 단어가 수록되어 있습니다.
+     * 사전에서 첫 번째 단어는 "A"이고, 그다음은 "AA"이며, 마지막 단어는 "UUUUU"입니다.
+     *
+     * 단어 하나 word가 매개변수로 주어질 때, 이 단어가 사전에서 몇 번째 단어인지 return 하도록 solution 함수를 완성해주세요.
+     */
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/84512
+
+    private static Dictionary<char, int> _vowelIndex = new Dictionary<char, int>()
+    {
+        { 'A', 0 },
+        { 'E', 1 },
+        { 'I', 2 },
+        { 'O', 3 },
+        { 'U', 4 },
+    };
+
+    private static int _length;
+
+    public static int Solution(string word)
+    {
+        _length =  word.Length;
+        
+        int index = 0;
+
+        for (int i = 0; i < _length; i++)
+            index += AddCalculatedIndexContribution(word[i], i);
+        
+        return index;
+    }
+
+    private static int AddCalculatedIndexContribution(char c, int index)
+    {
+        int contribution = 0;
+        
+        index = _length - index - 1;
+
+        for (int i = index; i >= 0; i--)
+        {
+            contribution += (int)Math.Pow(5, i);
+        }
+        
+        contribution *= _vowelIndex[c];
+
+        contribution++;
+        
+        return contribution;
+    }
+}
