d826a5bcea6115567469e58a8373c262fe761b27
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-08-21 17:46:04 +0900

    [feat] 명예의 전당 1 문제 풀이

--- Program.cs
+++ Program.cs
@@ -1,20 +1,13 @@
-﻿using PracticeCodeTest;
-using PracticeCodeTest.Solutions;
-using PracticeCodeTest.Solutions.Section1;
-using PracticeCodeTest.Solutions.Section2;
-
+﻿using PracticeCodeTest.Solutions;
 public class MainClass
 {
     static void Main(string[] args)
     {
-        var arr = PrivacyPolicyValidator.Solution
-        ("2022.05.19", 
-            ["A 6", "B 12", "C 3"],
-            ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]);
-        
+        var arr = HallOfFameManager.Solution(3, [10, 100, 20, 150, 1, 100, 200]);
+
         foreach (var item in arr)
-            {
+        {
             Console.WriteLine(item);
-            }
+        }
     }
 }
---
+++ Solutions/HallOfFameManager.cs
@@ -0,0 +1,46 @@
+﻿namespace PracticeCodeTest.Solutions;
+
+public class HallOfFameManager
+{
+    /*
+     * "명예의 전당"이라는 TV 프로그램에서는 매일 1명의 가수가 노래를 부르고,
+     * 시청자들의 문자 투표수로 가수에게 점수를 부여합니다.
+     * 매일 출연한 가수의 점수가 지금까지 출연 가수들의 점수 중 상위 k번째 이내이면
+     * 해당 가수의 점수를 명예의 전당이라는 목록에 올려 기념합니다.
+     * 즉 프로그램 시작 이후 초기에 k일까지는 모든 출연 가수의 점수가 명예의 전당에 오르게 됩니다.
+     * k일 다음부터는 출연 가수의 점수가 기존의 명예의 전당 목록의 k번째 순위의 가수 점수보다 더 높으면,
+     * 출연 가수의 점수가 명예의 전당에 오르게 되고 기존의 k번째 순위의 점수는 명예의 전당에서 내려오게 됩니다.
+     *
+     * 이 프로그램에서는 매일 "명예의 전당"의 최하위 점수를 발표합니다.
+     * 예를 들어, k = 3이고, 7일 동안 진행된 가수의 점수가 [10, 100, 20, 150, 1, 100, 200]이라면,
+     * 명예의 전당에서 발표된 점수는 아래의 그림과 같이 [10, 10, 10, 20, 20, 100, 100]입니다.
+     */
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/138477
+
+    public static int[] Solution(int k, int[] score)
+    {
+        List<int> scores = new List<int>();
+        Stack<int> minScores = new Stack<int>();
+
+        for (int i = 0; i < score.Length; i++)
+        {
+            scores.Add(score[i]);
+            
+            scores.Sort();
+            scores.Reverse();
+            
+            if (i < k-1)
+            {
+                minScores.Push(scores[i]);
+            }
+            else
+            {
+                minScores.Push(scores[k-1]);
+            }
+        }
+        
+        var answer = minScores.ToArray();
+        return answer.Reverse().ToArray();
+    }
+}
